eddbefca1f38c501f3413dd99eed833c3260819e
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-15 17:53:14 +0300

    Update project files

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './external-1.css';
+import './external-2.css';
+import './external-3.css';
+import './external-4.css';
+import './external-5.css';
+import './external-6.css';
+import './external-7.css';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
